e7287f6cb0e9bd74511d739be405cb10e2371572
sofiiimr <sofmaru.00@gmail.com>
2025-04-03 09:34:24 +0200

    add print steps in between

--- run_plot.py
+++ run_plot.py
@@ -3,8 +3,12 @@ import os
 
 # Step 1: Load data once (slow step)
 data_dir = "inputs"
+print(1)
 files = [os.path.join(data_dir, f) for f in os.listdir(data_dir) if f.endswith(".nc")]
+print(2)
 ds = load_dataset(files)
+print(3)
+print(ds)
 
 # Step 2: Plot whatever you want (fast)
 plot_wind_timeseries(ds, plot_speed=True, plot_direction=False)
